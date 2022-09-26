bf39fb1f7a8e8350ba59dfbe53ee74298fd6e28a
mashirnizamikpi <mashir.nizami@kpipartners.com>
2022-09-26 10:42:41 +0530

    My commit

---
+++ New folder/src/OOPS/Employer.java
@@ -0,0 +1,19 @@
+package OOPS;
+
+public class Employer {
+	
+	String Name;
+	int Salary;
+	int empID;
+	protected int getEmpID() {
+		return empID;
+	}
+	public int getSalary() {
+		return Salary;
+	}
+//	private int getName) {
+	//	return Name;
+	//}
+	
+
+}
