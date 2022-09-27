c5e44433a0bfb6dc940c5d42698aaadc3c9e08fe
mashirnizamikpi <mashir.nizami@kpipartners.com>
2022-09-27 11:57:03 +0530

    Commit

---
+++ Practice/src/Practice/TestConstructor.java
@@ -0,0 +1,18 @@
+package Practice;
+
+public class TestConstructor {
+public TestConstructor() {
+	int no0fLegs=2;
+}
+public TestConstructor(int no0fLegs,boolean fly) {
+	if(no0fLegs>2)
+		fly=false;
+}
+public static String TestConstructor() {
+	return "";
+}
+public static void main (String[] args) {
+
+	
+}
+}
