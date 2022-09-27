531e8b8d779bef57d6df639cb48b89251a4fb94c
mashirnizamikpi <mashir.nizami@kpipartners.com>
2022-09-27 13:54:51 +0530

    commit

--- Practice/src/Practice/TestConstructor.java
+++ Practice/src/Practice/TestConstructor.java
@@ -6,13 +6,13 @@ public TestConstructor() {
 }
 public TestConstructor(int no0fLegs,boolean fly) {
 	if(no0fLegs>2)
-		fly=false;
+		fly=true;
 }
 public static String TestConstructor() {
 	return "";
 }
 public static void main (String[] args) {
 
-	
+	System.out.println("I am Bright");
 }
 }
