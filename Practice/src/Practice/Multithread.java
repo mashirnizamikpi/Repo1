78039191f38ea19dcdf2f79c34f5a0c4328e3642
mashirnizamikpi <mashir.nizami@kpipartners.com>
2022-09-28 12:56:01 +0530

    Commit for thread

---
+++ Practice/src/Practice/Multithread.java
@@ -0,0 +1,24 @@
+package Practice;
+
+class Mutithread2 extends Thread{
+	public void run()
+	{
+
+}
+}
+public class Multithread {
+	public static void main(String[] args)
+	{
+		int n=8;
+		for (int i =0;i<n;i++) {
+			Multithread Object = new Multithread();
+			Object.start();
+		}
+	}
+
+	private void start() {
+		// TODO Auto-generated method stub
+		
+	}
+}
+	
