a0e2e8052dfe15732e434fa026b5da425c89375f
mashirnizamikpi <mashir.nizami@kpipartners.com>
2022-09-28 15:58:09 +0530

    Commit for default and interface

--- Practice/src/Practice/Sum.java
+++ Practice/src/Practice/Sum.java
@@ -3,27 +3,27 @@ package Practice;
 public interface Sum {
 	
 	//abstract method
-	public void addition(int b,int c);
+	public void addition(int a);
 	
-	default void show()
+	default void show(int b,int c)
 	{
+		int r=b+c;
 		System.out.println("Addtion performed successfully");//default method
 	}
 	
 class Add implements Sum
 {
-	public void addition(int b,int c) {
+	public void addition(int a) {
 		
-		System.out.println(b+c);
-		
+				
 	}
 	
 	public static void main (String args[])
 	{
 		Add a = new Add();
-		a.addition(1, 9);
+		a.addition(1);
 		
-		a.show();
+		a.show(9,11);
 	}	
 }
 	
