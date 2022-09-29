2a8731734606e91bc05fd9310b80b9c6a55884d4
mashirnizamikpi <mashir.nizami@kpipartners.com>
2022-09-29 17:55:18 +0530

    Commit for file handling

---
+++ Practice/src/Test/Test.java
@@ -0,0 +1,20 @@
+package Test;
+
+import java.util.*;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the value of a:");
+		int a=sc.nextInt();
+		System.out.println("Enter the value of b:");
+		int b=sc.nextInt();	
+		System.out.println("Enter the value of c:");
+		int c=sc.nextInt();	
+		int d=a+b+c;
+		System.out.println("Total:"+d);
+	}
+
+}
