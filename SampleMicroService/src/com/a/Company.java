58d08ee7eeb76baba67576e3aee55dd900450aa9
mashirnizamikpi <mashir.nizami@kpipartners.com>
2022-10-31 23:33:18 +0530

    Commit

---
+++ SampleMicroService/src/com/a/Company.java
@@ -0,0 +1,22 @@
+package com.a;
+
+public class Company {
+	
+	String com_name;
+	String com_add;
+	public String getCom_name() {
+		return com_name;
+	}
+	public void setCom_name(String com_name) {
+		this.com_name = com_name;
+	}
+	public String getCom_add() {
+		return com_add;
+	}
+	public void setCom_add(String com_add) {
+		this.com_add = com_add;
+	}
+
+
+
+}
